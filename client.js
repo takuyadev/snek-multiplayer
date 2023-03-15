0fc0f4889de04ce66f8de4f6ae4c3b441f450411
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-15 09:56:29 -0700

    started client

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("connected t server!!!!!!!!")
+    conn.write("Name: SNK")
+  })
+
+  conn.on("data", (data) => {
+    console.log(data);
+    conn.end();
+  });
+
+  conn.on("end", (data) => {
+    console.log("you end!!! ahhaha");
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
